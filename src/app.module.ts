edfc4a2ac28de658b0fd1f7d93fd37432387d64d
Josh Derocher <joshua.aarond@gmail.com>
2016-11-01 15:33:17 -0400

    removed material from master

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { MaterialModule } from '@angular/material';
 
 @NgModule({
     bootstrap: [ AppComponent ],
     declarations: [ AppComponent ],
     imports: [
-        BrowserModule,
-        MaterialModule.forRoot()
+        BrowserModule
     ],
     providers: [  ]
 })
